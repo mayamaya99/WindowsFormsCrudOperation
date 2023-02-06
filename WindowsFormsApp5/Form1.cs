216aaedd6c8901a384a88a013007324b7d01e61a
mayamaya99 <elmi33@hotmail.com>
2023-02-06 09:45:50 -0800

    Add delete command

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -22,7 +22,7 @@ namespace WindowsFormsApp5
         private void Form1_Load(object sender, EventArgs e)
         {
             string connectionString = null;
-           SqlConnection conn;
+            SqlConnection conn;
             SqlDataAdapter adapter = new SqlDataAdapter();
             DataSet ds = new DataSet();
             string sql = null;
@@ -31,13 +31,13 @@ namespace WindowsFormsApp5
             sql = "Select* from Bird order by BirdID";
             try
             {
-              
+
 
                 conn.Open();
                 adapter.SelectCommand = new SqlCommand();
                 adapter.SelectCommand.CommandText = sql;
                 adapter.SelectCommand.Connection = new SqlConnection();
-                adapter.SelectCommand.Connection.ConnectionString=connectionString;
+                adapter.SelectCommand.Connection.ConnectionString = connectionString;
                 adapter.Fill(ds);
 
                 //Start of Insert Block
@@ -55,14 +55,33 @@ namespace WindowsFormsApp5
                 adapter.InsertCommand.Parameters.Add("@Description", System.Data.SqlDbType.NVarChar, 24);
                 adapter.InsertCommand.Parameters["@Description"].SourceColumn = "Description";
 
-                //End of Insert Block
+                //Start of Update Block
+                adapter.UpdateCommand = new SqlCommand();
+                adapter.UpdateCommand.CommandText = "Update Bird" +
+
+                    "Set BirdID=@BirdID" +
+                    "Name=@Name" +
+                    "Description=@Description";
+                adapter.UpdateCommand.Connection = new SqlConnection();
+                adapter.UpdateCommand.Connection.ConnectionString = connectionString;
+                adapter.UpdateCommand.Connection = new SqlConnection();
+                adapter.UpdateCommand.Parameters.Add("@BirdID", System.Data.SqlDbType.NVarChar, 40);
+                adapter.UpdateCommand.Parameters["@BirdID"].SourceColumn = "BirdID";
+                adapter.UpdateCommand.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar, 40);
+                adapter.UpdateCommand.Parameters["@Name"].SourceColumn = "Name";
+                adapter.UpdateCommand.Parameters.Add("@Description", System.Data.SqlDbType.NVarChar, 24);
+                adapter.UpdateCommand.Parameters["@Description"].SourceColumn = "Description";
+                adapter.UpdateCommand.Connection = adapter.InsertCommand.Connection;
+
+                //Start of Delete Block
+                adapter.DeleteCommand = new SqlCommand();
+                adapter.DeleteCommand.CommandText = "Delete from Bird" +"Where  BirdID=@BirdID";
+                adapter.DeleteCommand.Parameters.Add("@BirdID", System.Data.SqlDbType.NVarChar, 40);
+                adapter.DeleteCommand.Parameters["@BirdID"].SourceColumn = "BirdID";
+                adapter.DeleteCommand.Connection = adapter.InsertCommand.Connection;
+
                 conn.Close();
                 dataGridView1.DataSource = ds.Tables[0];
-
-
-
-
-
 
             }
             catch (Exception ex)
